322663ff3b68f138018eb00dc072ad4087a064f7
chriscanyon <chrisnagy10@yahoo.com>
2018-10-26 12:14:07 -0500

    adding auto configuration

--- Sensor_V_MPPT_Estimator.py
+++ Sensor_V_MPPT_Estimator.py
@@ -5,6 +5,24 @@ import scipy.io as sio
 import numpy
 import math
 
+def array_increment(array, N)
+    size = array.size
+    for i in range(array.size)
+        if array[size - 1 - i] == N
+            #check if done
+            if size-i = 0
+                #done
+                return array
+            else
+                #else roll over to 0
+                array[size-i] = 0
+        else 
+        # add one and done
+        array[size - 1 - i] += 1
+        return array
+
+
+
 def evaluate_model(model, testdatafile):
     testdata = sio.loadmat(testdatafile)
 
@@ -25,7 +43,7 @@ def evaluate_model(model, testdatafile):
     print('avg error in Volts for test data: %s' % testdatafile)
     print(averageErrorVolts)
 
-def train_model(layers, dataset):
+def train_model(layers, dataset, name):
     data = sio.loadmat(dataset)
 
     X = numpy.array(data['inputs'])
@@ -41,31 +59,35 @@ def train_model(layers, dataset):
 
     model.compile(loss='mean_squared_error', optimizer='sgd', metrics=['accuracy'])
 
-    model.fit(X, Y, epochs=1000, batch_size=10000)
+    model.fit(X, Y, epochs=1000, batch_size=10000, verbose=0)
 
     # serialize model to JSON
     model_json = model.to_json()
-    with open("model.json", "w") as json_file:
+    with open(name+".json", "w") as json_file:
         json_file.write(model_json)
     # serialize weights to HDF5
-    model.save_weights("model.h5")
+    model.save_weights(name+".h5")
     print("Saved model to disk")
 
     return model
 
+L = 3
+N = 10
+
+layers = [15,15,15,15]
+name = 'model4x15'
 try:
     # load json and create model
-    json_file = open('model3x7.json', 'r')
+    json_file = open(name + '.json', 'r')
     loaded_model_json = json_file.read()
     json_file.close()
     model = model_from_json(loaded_model_json)
     # load weights into new model
-    model.load_weights("model.h5")
+    model.load_weights(name + ".h5")
     print("Loaded model from disk")
     model.compile(loss='mean_squared_error', optimizer='sgd', metrics=['accuracy'])
 except OSError as e:
     print('No saved model, training new one')
-    layers = [7,7,7]
-    model = train_model(layers, 'dataset10k.mat')
+    model = train_model(layers, 'dataset10k.mat', name)
 
 evaluate_model(model, 'dataset1k.mat')
