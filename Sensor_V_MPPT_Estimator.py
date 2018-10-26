90738391e60d03983eabd4128a0cb05eaba8dd18
Chris Nagy <chrisnagy10@yahoo.com>
2018-10-26 09:22:14 -0500

    models now save ro reuse.

--- Sensor_V_MPPT_Estimator.py
+++ Sensor_V_MPPT_Estimator.py
@@ -1,38 +1,72 @@
 import keras
+from keras.models import model_from_json
 from keras.layers import Activation, Dense
 import scipy.io as sio
 import numpy
+import math
 
-data = sio.loadmat('dataset10k.mat')
+def evaluate_model(model, testdatafile):
+    testdata = sio.loadmat(testdatafile)
 
-X = numpy.array(data['inputs'])
-Y = numpy.array(data['labels'][0])
+    testX = numpy.array(testdata['inputs'])
+    testY = numpy.array(testdata['labels'][0])
 
-layers = [6,4,3]
+    predictions = model.predict(testX)
+    flat_list = [item for sublist in predictions for item in sublist]
 
-model = keras.Sequential()
-model.add(Dense(9, input_dim=2, activation='sigmoid'))
+    print('avg error in Volts for test data: %s' % testdatafile)
+    totalError = 0
+    i = 0
+    for p in flat_list:
+        error = abs(testY[i] - p)
+        totalError += math.pow(error, 2)
+        i += 1
+    mse = (totalError/i)
+    print("Preditions: ",flat_list)
+    print("Actual: ", testY)
+    print(mse)
 
-for nodes in layers:
-    model.add(Dense(nodes, activation='sigmoid'))
+def train_model(layers, dataset):
+    data = sio.loadmat(dataset)
 
-model.add(Dense(1))
+    X = numpy.array(data['inputs'])
+    Y = numpy.array(data['labels'][0])
 
-model.compile(loss='mean_squared_error', optimizer='sgd', metrics=['accuracy'])
+    model = keras.Sequential()
+    model.add(Dense(layers[0], input_dim=2, activation='sigmoid'))
 
-model.fit(X, Y, epochs=1000, batch_size=10000)
+    for nodes in layers[1:]:
+        model.add(Dense(nodes, activation='sigmoid'))
 
-scores = model.evaluate(X, Y)
+    model.add(Dense(1))
 
-print("\n%s: %.2f%%" % (model.metrics_names[1], scores[1]*100))
+    model.compile(loss='mean_squared_error', optimizer='sgd', metrics=['accuracy'])
 
-testdata = sio.loadmat('dataset.mat')
+    model.fit(X, Y, epochs=1000, batch_size=10000)
 
-testX = numpy.array(data['inputs'])
-testY = numpy.array(data['labels'][0])
+    # serialize model to JSON
+    model_json = model.to_json()
+    with open("model.json", "w") as json_file:
+        json_file.write(model_json)
+    # serialize weights to HDF5
+    model.save_weights("model.h5")
+    print("Saved model to disk")
 
-predictions = model.predict(testX)
-print("Predictions: ")
-print(predictions)
-print("Actual: ")
-print(testY)
+    return model
+
+try:
+    # load json and create model
+    json_file = open('model.json', 'r')
+    loaded_model_json = json_file.read()
+    json_file.close()
+    model = model_from_json(loaded_model_json)
+    # load weights into new model
+    model.load_weights("model.h5")
+    print("Loaded model from disk")
+    model.compile(loss='mean_squared_error', optimizer='sgd', metrics=['accuracy'])
+except OSError as e:
+    print('No saved model, training new one')
+    layers = [9,6,4,3]
+    model = train_model(layers, 'dataset10k.mat')
+
+evaluate_model(model, 'dataset.mat')
